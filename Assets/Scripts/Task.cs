bcdac1dac5490ebf80f5c2f377d4eee1326617b9
Tiny <12611939+tinycrate@users.noreply.github.com>
2020-11-15 18:11:00 +0000

    Initial Commit

---
+++ Assets/Scripts/Task.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Task {
+    String Name { get; }
+    List<Feature> Features { get; }
+    event EventHandler OnTaskCompleted;
+}
