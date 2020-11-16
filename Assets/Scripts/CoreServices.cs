89942fd49cde3dabe59a020c85eec79cb3b37b24
Tiny <abcdo39.ronald@gmail.com>
2020-11-16 19:12:17 +0000

    Added some service related classes

---
+++ Assets/Scripts/CoreServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class CoreServices : MonoBehaviour, DeployableService {
+    public bool Deployed { get; } = true;
+
+    public void TickDay() {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ Assets/Scripts/MonoBehaviourSingleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+    private static object instance;
+    public static T GetInstance => (T) instance;
+
+    void Awake () {
+        if(GetInstance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Debug.LogError($"Attempted to initialize a {this.GetType().Name} that already exist in the game, destroying...");
+            Destroy(this);
+        }
+    }
+
+}
