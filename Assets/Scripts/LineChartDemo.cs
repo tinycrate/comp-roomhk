784a7d26d1fd3afb3e410e2e8e93ce0559dd388a
Sikyin Tang <thomas2d19@gmail.com>
2020-11-16 15:48:53 +0000

    Pushed a line chart demo!

---
+++ Assets/Scripts/LineChartDemo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XCharts;
+
+public class LineChartDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var chart = gameObject.GetComponent<LineChart>();
+        chart.title.show = true;
+        chart.title.text = "Line Chart Script Demo";
+        for (int i = 0; i < 10; i++)
+        {
+            chart.AddXAxisData("x" + i);
+            chart.AddData(0, Random.Range(10, 20));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
