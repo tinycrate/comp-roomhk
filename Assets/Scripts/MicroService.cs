32a95c2816625f0109cd17c48b72bf2a844de026
Tiny <12611939+tinycrate@users.noreply.github.com>
2020-11-16 19:12:17 +0000

    Added some service related classes

---
+++ Assets/Scripts/MicroService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+class MicroService : Task, DeployableService {
+    public string Name { get; }
+    public List<Feature> Features { get; } = new List<Feature>();
+    public event EventHandler OnTaskCompleted;
+    public bool Deployed { get; }
+
+    public float EndProductQuality {
+        get {
+            if (!Deployed || Features.Count <= 0) return 0;
+            if (Features.Any(x => x.CurrentState != Feature.State.Merged)) return 0;
+            return Features.Sum(x => x.EndProductQuality) / Features.Count;
+        }
+    }
+
+    public void TickDay() {
+        if (!Deployed) return;
+    }
+}
