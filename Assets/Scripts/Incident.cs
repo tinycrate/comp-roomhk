c0a6d7860fa1602f28b48d4ad16d6a801d2d61b8
Chow Chung Yu <chow.jccy@gmail.com>
2020-11-18 13:35:42 +0000

    + incident prefab, update the other 2

---
+++ Assets/Scripts/Incident.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Incident : MonoBehaviour
+{
+    private const int SIZE = 4;
+
+    //Options
+    [Header("Options")]
+    public Text[] option = new Text[SIZE];
+    public string[] option_text = new string[SIZE];
+
+    void OnValidate() {
+        if (option.Length != SIZE) { Array.Resize(ref option, SIZE); }
+        if (option_text.Length != SIZE) { Array.Resize(ref option_text, SIZE); }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 4; i++) {
+            option[i].text = option_text[i];
+        }
+    }
+}
