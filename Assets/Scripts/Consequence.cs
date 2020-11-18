9f094cb7c404fb5a8ffbbe6c78cf0ea5f64969d4
Chow Chung Yu <chow.jccy@gmail.com>
2020-11-18 12:41:57 +0000

    update develop, add consequence prefab

---
+++ Assets/Scripts/Consequence.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Consequence : MonoBehaviour
+{
+    public Text consequence_text;
+    [SerializeField] private string consequence_input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        consequence_input = "Lorem ipsum dolor sit amet, at primis nostrum fabellas pri, \\" +
+            "ea delectus placerat disputationi sit, ne vel novum iisque euismod. Saperet torquatos ut sed. \\" +
+            "Ex vim purto platonem. Et pri feugiat similique, iudico volutpat assentior ut mei. Mea ut \\" +
+            "nominati reformidans, per adhuc deleniti eu, vix no dico delectus erroribus.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        consequence_text.text = consequence_input;
+    }
+}
--- Assets/Scripts/Developing.cs
+++ Assets/Scripts/Developing.cs
@@ -7,46 +7,49 @@ using System;
 public class Developing : MonoBehaviour
 {
     private const int SIZE = 3;
-    private string[] tick_cross_option = { "✗", "✓" };
+    private string[] tick_cross_string = { "✗", "✓" };
+    public enum tick_cross_option { none, tick, cross};
 
     // Task Name
     [Header("Task Name")]
     public Text task_name;
-    [SerializeField] private string task_text;
+    public string task_text;
 
     // Feature Name
     [Header("Task Name")]
     public Text[] feature;
-    [SerializeField] private string[] feature_text = new string[SIZE];
+    public string[] feature_text = new string[SIZE];
 
     // Deploy and Release
     [Header("Deploy and Release")]
     public Image release_bar;
     public Image deploy_bar;
-    [Range(0, 1)] [SerializeField] private float deploy_progress;
-    [Range(0, 1)] [SerializeField] private float release_progress;
+    [Range(0, 1)] public float deploy_progress = 0;
+    [Range(0, 1)] public float release_progress = 0;
 
     // Code progress
     [Header("Code")]
     public Text[] code = new Text[SIZE];
-    [Range(0, 100)] [SerializeField] private int[] code_progress = new int[SIZE];
+    [Range(0, 1)] public float[] code_progress = new float[SIZE];
 
     // Build progress
     [Header("Build")]
     public Text[] build = new Text[SIZE];
-    [SerializeField] private bool[] build_progress = new bool[SIZE];
+    public tick_cross_option[] build_progress = new tick_cross_option[SIZE];
+    //public bool[] build_progress = new bool[SIZE];
     
     // Test progress
     [Header("Test")]
     public Text[] test = new Text[SIZE];
-    [Range(0, 100)] [SerializeField] private int[] test_progress = new int[SIZE];
-    [SerializeField] private bool[] test_pass = new bool[SIZE];
+    [Range(0, 1)] public float[] test_progress = new float[SIZE];
+    public bool[] test_pass = new bool[SIZE];
 
 
     // Merge progress
     [Header("Merge")]
     public Text[] merge = new Text[SIZE];
-    [SerializeField] private bool[] merge_progress = new bool[SIZE];
+    public tick_cross_option[] merge_progress = new tick_cross_option[SIZE];
+    //public bool[] merge_progress = new bool[SIZE];
 
 
     void OnValidate() {
@@ -63,19 +66,6 @@ public class Developing : MonoBehaviour
         if (merge_progress.Length != SIZE) { Array.Resize(ref merge_progress, SIZE); }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        for (int i = 0; i < 3; i++) {
-            code[i].text = "0%";
-            test[i].text = "0%";
-            build_progress[i] = false;
-            merge_progress[i] = false;
-            test_pass[i] = true;
-        }
-        task_text = "default task name";
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -88,16 +78,18 @@ public class Developing : MonoBehaviour
 
         for (int i = 0; i < 3; i++)
         {
-            code[i].text = code_progress[i] + "%"; //code
+            code[i].text = (int)(code_progress[i] * 100) + "%"; //code
 
-            if (test_pass[i] == true) { test[i].text = test_progress[i] + "%"; }
-            else { test[i].text = tick_cross_option[0]; } //test
+            if (test_pass[i] == false) { test[i].text = (int)(test_progress[i] * 100) + "%"; }
+            else { test[i].text = tick_cross_string[0]; } //test
 
-            if (build_progress[i] == false) { build[i].text = tick_cross_option[0]; }
-            else { build[i].text = tick_cross_option[1]; } //build
+            if (build_progress[i] == tick_cross_option.cross) { build[i].text = tick_cross_string[0]; }
+            else if (build_progress[i] == tick_cross_option.tick) { build[i].text = tick_cross_string[1]; }
+            else if (build_progress[i] == tick_cross_option.none) { build[i].text = " "; } //build
 
-            if (merge_progress[i] == false) { merge[i].text = tick_cross_option[0]; }
-            else { merge[i].text = tick_cross_option[1]; } //merge
+            if (merge_progress[i] == tick_cross_option.cross) { merge[i].text = tick_cross_string[0]; }
+            else if (merge_progress[i] == tick_cross_option.tick) { merge[i].text = tick_cross_string[1]; }
+            else if (merge_progress[i] == tick_cross_option.none) { merge[i].text = " "; } //merge
 
             feature[i].text = feature_text[i]; //feature name
         }
