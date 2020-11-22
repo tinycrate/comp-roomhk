80dae52c262d11b55c36fb20d25ebbd27ccbd421
Tiny <12611939+tinycrate@users.noreply.github.com>
2020-11-22 19:55:44 +0000

    60% playable 70% demoable

--- Assets/Scripts/UI/MainGame/DeployableTaskEntryDeployedController.cs
+++ Assets/Scripts/UI/MainGame/DeployableTaskEntryDeployedController.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using UnityEditor;
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class DeployableTaskEntryDeployedController : MonoBehaviour, ITaskEntryController {
--- Assets/Scripts/UI/MainGame/ProductionEntryController.cs
+++ Assets/Scripts/UI/MainGame/ProductionEntryController.cs
@@ -3,10 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using UnityEditor;
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ProductionEntryController : MonoBehaviour {
